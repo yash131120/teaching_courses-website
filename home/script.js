36826fab4fa86b8730e2a5447ae930b157389c70
yash131120 <139432375+yash131120@users.noreply.github.com>
2023-12-11 15:03:14 +0530

    teaching_courses-website

---
+++ home/script.js
@@ -0,0 +1,24 @@
+    /*===================== scroll revel =====================*/
+    ScrollReveal({
+        reset: true,
+        distance : '80px',
+        duration : 2000,
+        delay : 200
+    });
+
+    ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+    ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, ,contact form', { origin: 'bottom' });
+    ScrollReveal().reveal('.home-content h1, .about-img',{ origin: 'left' });
+    ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+    
+    /*===================== typed js =====================*/
+    const typed = new Typed('.multiple-text', {
+        strings: ['Electricians, Plumbers, ', ' Mechanics, Carpenters, ', ' Painters, Drivers....'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+
+
